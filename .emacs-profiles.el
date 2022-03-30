801304310612f7b4addfc8c0d5dab5ead7318236
Jan Hendron <rhbhendron@gmail.com>
2022-03-30 02:26:49 +0100

    Added EXWM config fork

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,1 +1,4 @@
-(("default" . ((user-emacs-directory . "~/.doom-emacs")))) 
+(("default" . ((user-emacs-directory . "~/.doom-emacs")
+               (env (("DOOMDIR" . "~/.doom.d")))))
+ ("exwm" . ((user-emacs-directory . "~/.exwm-doom-emacs")
+            (env (("DOOMDIR" . "~/.exwm-doom.d"))))))
